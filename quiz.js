887408537b25787397c4f5e0070ae59ce32c20e8
Ali Lahrime <alahrime@email.wm.edu>
2019-01-22 16:38:53 -0500

    changes made to index.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-let questionNumber = 1;
+let questionNumber = 0;
 let points = 0;
 
 function generateHtml() {
@@ -14,6 +14,33 @@ function generateHtml() {
   }
 }
 
-generateStart() {
-  
-}
+//function evaluateAnswer() {
+  if (
+  questionNumber++;
+  $('.questionNumber').text(questionNumber + 1);
+//}
+
+
+generateQuestion() {
+	
+return     `<article class="content">
+	    <h3>${STORE[questionNumber].question</h3>
+ 	    <form id="multipleChoice">
+	    <label class="answerOption">
+ 	    <input type="text" value="${STORE[questionNumber].answers[0]} required>
+	    <span>${STORE[questionNumber].answers[0]}</span>
+	    </label>
+	    <label class="answerOption">
+	    <input type="radio" value="${STORE[questionNumber].answers[1]}" required>   	   <span>${STORE[questionNumber].answers[1]}</span>
+	    </label>
+	    <label class="answerOption">
+            <input type="radio" value="${STORE[questionNumber].answers[2]}" required>              <span>${STORE[questionNumber].answers[2]}</span>
+            </label>
+	    <label class="answerOption">
+            <input type="radio" value="${STORE[questionNumber].answers[3]}" required>              <span>${STORE[questionNumber].answers[3]}</span>
+            </label>
+	    <button type="submit">Submit Answer</button>
+	    </form>
+            </article>`
+
+
